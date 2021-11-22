a12b8831074cc022976c8e69dd9f5f93f4f90708
Ksenia.Ilina <ksenia.ilina@jetbrains.com>
2021-11-22 19:08:15 +0100

    commit

---
+++ src/test/java/com/company/SomeTest.java
@@ -0,0 +1,17 @@
+package test.java.com.company;
+
+import org.junit.Test;
+
+public class SomeTest {
+    //selection start
+    @Test
+    public void testFoo() {
+        System.out.println("foo");
+    }
+
+    @Test
+    public void testBar() {
+        System.out.println("bar");
+    }
+    //selection end
+}
